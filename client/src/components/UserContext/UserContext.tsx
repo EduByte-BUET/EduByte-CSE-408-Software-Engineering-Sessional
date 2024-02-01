5c63e547143e94ecf8649331e927e3632bf597ec
marzan <nahimarzan02@gmail.com>
2024-02-01 12:12:28 +0600

    Homepage, content-creator, user-dashboard

--- client/src/components/UserContext/UserContext.tsx
+++
@@ -1,6 +0,0 @@
-import React, { Dispatch, SetStateAction } from 'react';
-
-export const UserContext = React.createContext({
-  currentUser: null,
-  setCurrentUser: (() => {}) as Dispatch<SetStateAction<any>>,
-});
